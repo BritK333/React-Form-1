cb6503042b11dd1f3e304f4ab7469e233621eeca
BritK333 <31315388+BritK333@users.noreply.github.com>
2019-12-29 19:22:42 -0700

    added new line

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.firstName}
+          name="firstName"
+          placeholder="First Name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          value={this.state.lastName}
+          name="lastName"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+        />
+        <h2>
+          {this.state.firstName} {this.state.lastName}
+        </h2>
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,6 +64,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
